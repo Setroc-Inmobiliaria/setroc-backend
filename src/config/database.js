df5981e53deb36c3701a72f5e45d2cf297edbfaf
Asael Hernandez <zanduva@gmail.com>
2024-01-08 18:48:32 -0600

    go

--- src/config/database.js
+++ src/config/database.js
@@ -4,11 +4,11 @@ const dotenv = require("dotenv")
 dotenv.config()
 
 const sequelize = new Sequelize({
-    database: "setroc",
-    username:"setroc",
-    password:process.env.PASSWD,
-    host: "dpg-cme98nmn7f5s73fom7q0-a",
-    dialect: "postgres"
+    database: process.env.DB_NAME || "setroc", // Cambiado para usar variable de entorno o valor predeterminado
+    username: process.env.DB_USER || "setroc", // Cambiado para usar variable de entorno o valor predeterminado
+    password: process.env.DB_PASS || process.env.PASSWD, // Cambiado para usar variable de entorno o valor predeterminado
+    host: process.env.DB_HOST || "dpg-cme98nmn7f5s73fom7q0-a", // Cambiado para usar variable de entorno o valor predeterminado
+    dialect: "postgres",
 })
 
 module.exports = sequelize 
